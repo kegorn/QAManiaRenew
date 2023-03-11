82511fedda7e399ae0418734f2258fa0c301e141
Egor Kobelev <ekobelev@amchealth.com>
2023-03-10 21:38:34 -0800

    Playwright Python: 07 Selectors - add changes to start debug

--- test_testcases.py
+++ test_testcases.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, devtools=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto("http://127.0.0.1:8000/login/?next=/")
@@ -20,6 +20,10 @@ def run(playwright: Playwright) -> None:
     page.get_by_label("Test description").fill("world")
     page.get_by_role("button", name="Create").click()
     page.get_by_role("link", name="Test Cases").click()
+
+    page.goto("http://127.0.0.1:8000/tests/")
+
+    page.locator('xpath=//td[contains(text(),"hello")]').click()
 
     assert page.locator('xpath=//td[contains(text(),"hello")]') is not None
 
