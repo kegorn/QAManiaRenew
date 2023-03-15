62e6d706c3c9024043da54e161b944623a01913c
Egor Kobelev <ekobelev@amchealth.com>
2023-03-15 13:26:42 -0700

    Playwright Python: 12 Configuration - 5 .evn variables

--- conftest.py
+++ conftest.py
@@ -1,9 +1,7 @@
 from pytest import fixture
 from playwright.sync_api import sync_playwright
-
 import settings
 from page_objects.application import App
-
 import os
 import json
 
@@ -23,11 +21,11 @@ def get_playwright():
 
 @fixture(scope='session')
 def desktop_app(get_playwright, request):
-    # base_url = request.config.getoption('--base_url')
+    base_url = request.config.getoption('--base_url')
     # 3
     # base_url = request.config.getini('base_url')
     # 4
-    base_url = request.config.getoption('--base_url')
+    # base_url = request.config.getoption('--base_url')
     app = App(get_playwright, base_url=base_url, devtools=False)
     app.goto('/')
     yield app
@@ -35,26 +33,28 @@ def desktop_app(get_playwright, request):
 
 
 @fixture(scope='session')
-def desktop_app_auth(desktop_app, request):
-    secure = request.config.getoption('--secure')
-    config = load_config(secure)
+def desktop_app_auth(desktop_app):
+    # secure = request.config.getoption('--secure')
+    # config = load_config(secure)
+    account = {'username': os.environ['USER'],
+               'password': os.environ['PASSWORD']}
 
     desktop_app = desktop_app
     desktop_app.goto('/login')
-    desktop_app.login(**config)
+    desktop_app.login(**account)
     yield desktop_app
-    # app.close()
 
 
 # hook
 def pytest_addoption(parser):
-    # parser.addoption('--base_url', action='store', default='http://127.0.0.1:8000')
+    parser.addoption('--base_url', action='store', default='http://127.0.0.1:8000')
     # parser.addini('base_url', help='base url of site under test', default='http://127.0.0.1:8000')
     # 4
-    parser.addoption('--base_url', action='store', default='http://127.0.0.1:8000')
-    parser.addoption('--secure', action='store', default='secure.json')
+    # parser.addoption('--base_url', action='store', default='http://127.0.0.1:8000')
+    # parser.addoption('--secure', action='store', default='secure.json')
 
-def load_config(file):
-    config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
-    with open(config_file) as cfg:
-        return json.loads(cfg.read())
+
+# def load_config(file):
+#     config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+#     with open(config_file) as cfg:
+#         return json.loads(cfg.read())
